5e8d948231894f877994d1c8b155ae8775b277d7
rc2pc2 <petriccarcc@gmail.com>
2024-10-07 10:49:09 +0200

    removed comments

--- snack-4/index.php
+++ snack-4/index.php
@@ -1,21 +1,4 @@
 <?php 
-
-// @here
-// Ciao ragazze e ciao ragazzi!
-// Descrizione
-// Continuiamo a lavorare nella repo che abbiamo usato questa mattina per gli snack, ma in una nuova cartella (snack-4), ovvero: nome repo: php-snacks-b1
-// Dato questo array di classi con studenti: https://pastebin.com/HksnN0aS
-// Snack 4A:
-// Stampare in pagina, senza particolare stilizzazione il nome di ogni classe e sotto ognuna, i dati di ogni studente/studentessa presente nella relativa classe.
-// Snack 4B:
-// Filtrare il nostro array e mostrare, sempre suddivisi per classe, esclusivamente gli studenti e le studentesse con voto medio sufficiente.
-// Snack 4C:
-// Commentiamo il filtro del punto 4B (lasciatelo nel codice, in modo che possiamo comunque vederlo) e aggiungiamo un minimo di stile a piacere. Inseriamo quindi un form in cui l’utente possa inserire un input con il voto medio massimo, e filtriamo in base ad esso solo gli studenti che abbiano una media esclusivamente inferiore al voto inserito.
-// Snack 4D:
-// Aggiungiamo un input che consenta l’inserimento del linguaggio di programmazione preferito, e filtriamo in base ad esso solo gli studenti che abbiano quel linguaggio come preferito.
-// Bonus:
-// Implementare funzionalita’ di filtraggio aggiuntive basate, ad esempio, su nome, cognome o anni dei relativi studenti, o magari sul nome della nostra classe.
-
 $classes = [
     "Classe 1A" => [
         [
